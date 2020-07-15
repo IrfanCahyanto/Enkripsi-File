de38874a86257ab4278986ad0a91a61a5bb81621
1817101438 <1817101438@ASX441U-E8AN1EE>
2020-07-15 21:47:05 +0700

    Upload Project Perbaikan

---
+++ src/Enkripsi.java
@@ -0,0 +1,72 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 1817101438
+ */
+public class Enkripsi {
+    public static void enkripsi(File src, File dest)
+            throws IOException {
+
+        if (src.isDirectory()) {
+
+            //if directory not exists, create it
+            if (!dest.exists()) {
+                dest.mkdirs();
+                System.out.println("Directory copied from "
+                        + src + "  to " + dest);
+            }
+
+            //list all the directory contents
+            String files[] = src.list();
+
+            for (String file : files) {
+                //construct the src and dest file structure
+                File srcFile = new File(src, file);
+                File destFile = new File(dest, file);
+                //recursive copy
+                enkripsi(srcFile, destFile);
+            }
+
+        } else {
+            //if file, then copy it
+            //Use bytes stream to support all file types
+            InputStream in = new FileInputStream(src);
+            OutputStream out = new FileOutputStream(dest);
+
+            byte[] buffer = new byte[1024];
+
+            int length;
+            int key=3;
+            boolean done = false;
+            //copy the file content in bytes 
+            while ((length = in.read()) > 0) {
+                if(length==-1){
+                    done=true;
+                } else {
+                    byte b = (byte) length;
+                    byte c = (byte) (b+key);
+                    out.write(c);
+                }
+            }
+
+            in.close();
+            out.close();
+            System.out.println("File copied from " + src + " to " + dest);
+        }
+    }
+    
+    
+ 
+}
